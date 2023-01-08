4c8d3868c165119f4b43c607cc32638d4debb1eb
Cody Peters <cjosephp15@gmail.com>
2023-01-08 17:33:01 +1000

    Added icons and tooltips for tags on GUI

--- GUI/static/js/script.js
+++ GUI/static/js/script.js
@@ -1,3 +1,12 @@
+// Tooltips
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="tooltip"]')
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
+
+// Edit video content
 const remakeBtn = $("#remakeBtn");
 
 const originalThread = { comments: {} };
